325c22b79353604d4df74ed3ff768b434e22eb76
karasikrus <karasikrus@gmail.com>
2019-11-13 00:44:26 +0300

    store saves/loads in/from local storage

--- src/store.js
+++ src/store.js
@@ -1,4 +1,16 @@
-import { createStore } from "redux";
+import {createStore} from "redux";
 import rootReducer from "./reducers/index";
-const store = createStore(rootReducer);
+
+
+const persistedState = localStorage.getItem('reduxState') ? JSON.parse(localStorage.getItem('reduxState'))
+    : {
+        cities: []
+    };
+const store = createStore(
+    rootReducer,
+    persistedState,
+);
+store.subscribe(() => {
+    localStorage.setItem('reduxState', JSON.stringify(store.getState()))
+});
 export default store;
