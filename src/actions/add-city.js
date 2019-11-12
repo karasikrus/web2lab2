62a7a96af95d33b0fbc443f0c263680fad915c87
karasikrus <karasikrus@gmail.com>
2019-11-12 22:24:39 +0300

    useless store works

---
+++ src/actions/add-city.js
@@ -0,0 +1,5 @@
+import {ADD_CITY} from "./action-types";
+
+export function addCity(payload) {
+    return {type: ADD_CITY, payload}
+}
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,15 @@
+import {ADD_CITY} from "../actions/action-types";
+
 const initialState = {
     cities: []
 };
 
 function rootReducer(state = initialState, action) {
+    if(action.type === ADD_CITY){
+        return Object.assign({}, state, {
+            cities: state.cities.concat(action.payload)
+        });
+    }
     return state;
 };
 
